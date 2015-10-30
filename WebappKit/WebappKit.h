88e95e99e9eb50e1d9ed84feb5d03467afcd6b9f
Jason Brennan <i.jasonbrennan@gmail.com>
2015-10-30 18:42:40 -0400

    Initial Commit

---
+++ WebappKit/WebappKit.h
@@ -0,0 +1,19 @@
+//
+//  WebappKit.h
+//  WebappKit
+//
+//  Created by Jason Brennan on 2015-10-30.
+//  Copyright © 2015 Jason Brennan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for WebappKit.
+FOUNDATION_EXPORT double WebappKitVersionNumber;
+
+//! Project version string for WebappKit.
+FOUNDATION_EXPORT const unsigned char WebappKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebappKit/PublicHeader.h>
+
+
